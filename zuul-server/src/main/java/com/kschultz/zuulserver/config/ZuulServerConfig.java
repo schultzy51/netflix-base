b2f46dbfe3d7038fa7c5ca801de8691c22054420
Ken Schultz <schultzy51@gmail.com>
2015-01-21 00:08:49 -0600

    Test Pre Zuul Filter

--- zuul-server/src/main/java/com/kschultz/zuulserver/config/ZuulServerConfig.java
+++ zuul-server/src/main/java/com/kschultz/zuulserver/config/ZuulServerConfig.java
@@ -1,11 +1,16 @@
 package com.kschultz.zuulserver.config;
 
+import com.kschultz.zuulserver.filter.pre.TestPreZuulFilter;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableZuulProxy
 //@EnableZuulServer
 @Configuration
 public class ZuulServerConfig {
+    @Bean
+    TestPreZuulFilter testPreZuulFilter() {
+        return new TestPreZuulFilter();
+    }
 }
