0e99e502a340fefb7eaf354023936ff044255f7b
Ken Schultz <schultzy51@gmail.com>
2015-01-22 23:30:56 -0600

    somewhat working config, eureka replication is tricky

--- main/src/main/java/com/kschultz/mainapp/web/controller/TestController.java
+++ main/src/main/java/com/kschultz/mainapp/web/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.kschultz.mainapp.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
     @RequestMapping("/")
     public String home() {
+        log.info("This node!");
         return "test controller";
     }
 }
