d1438ee74262fb6f7cdfe948844c7d40a6b220d8
Ken Schultz <schultzy51@gmail.com>
2015-02-24 20:35:06 -0600

    Upgrade deps, working on Dockerfile for eureka

--- zuul/src/main/java/com/kschultz/zuulserver/filter/pre/TestPreZuulFilter.java
+++ zuul/src/main/java/com/kschultz/zuulserver/filter/pre/TestPreZuulFilter.java
@@ -1,11 +1,8 @@
 package com.kschultz.zuulserver.filter.pre;
 
-import com.netflix.zuul.FilterProcessor;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.Validate;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 
